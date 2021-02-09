470f8fd639adfa095c032bac3be6c48ef9a720de
secbreel <gloom.19932005@gmail.com>
2021-02-09 14:14:54 +0300

    added manual dark mode changing

--- app/src/main/java/com/secbreel/notes/ui/SettingsFragment.kt
+++ app/src/main/java/com/secbreel/notes/ui/SettingsFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.secbreel.notes.R
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -16,17 +18,31 @@ class SettingsFragment : Fragment() {
     private val viewModel by viewModel<SettingsFragmentViewModel>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_settings, container, false)
 
         rootView.findViewById<Button>(R.id.clearCategories).setOnClickListener {
             viewModel.categoryRepositoryClear
-                .subscribe()
+                    .subscribe()
             viewModel.notesRepositoryClear
-                .subscribe()
+                    .subscribe()
             Toast.makeText(requireContext(), "CATEGORIES CLEARED!", Toast.LENGTH_SHORT).show()
+        }
+
+        rootView.findViewById<RadioGroup>(R.id.themeChangingRadioGroup).setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.systemThemeSettingsButton -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                }
+                R.id.lightThemeSettingsButton -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+                R.id.darkThemeSettingsButton -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                }
+            }
         }
 
 
