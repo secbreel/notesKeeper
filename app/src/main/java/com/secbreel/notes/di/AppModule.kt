a4d3bda3f74edbfe0c1c920f768128f219c37ee4
secbreel <gloom.19932005@gmail.com>
2021-04-07 17:57:41 +0300

    databases are united now

--- app/src/main/java/com/secbreel/notes/di/AppModule.kt
+++ app/src/main/java/com/secbreel/notes/di/AppModule.kt
@@ -1,10 +1,7 @@
 package com.secbreel.notes.di
 
 import androidx.room.Room
-import com.secbreel.notes.persistance.CategoryDatabase
-import com.secbreel.notes.persistance.CategoryRepository
-import com.secbreel.notes.persistance.NoteDatabase
-import com.secbreel.notes.persistance.NoteRepository
+import com.secbreel.notes.persistance.*
 import com.secbreel.notes.ui.calendar.CalendarViewModel
 import com.secbreel.notes.ui.categories_list.CategoriesListViewModel
 import com.secbreel.notes.ui.category_screen.CategoryScreenViewModel
@@ -18,18 +15,12 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val app = module {
-    single { get<CategoryDatabase>().categoryDao() }
-    single { get<NoteDatabase>().noteDao() }
+    single { get<AppDatabase>().categoryDao() }
+    single { get<AppDatabase>().noteDao() }
     single {
         Room.databaseBuilder(
-            androidContext(), CategoryDatabase::class.java,
-            "categoriesDatabase"
-        ).build()
-    }
-    single {
-        Room.databaseBuilder(
-            androidContext(), NoteDatabase::class.java,
-            "notesDatabase"
+            androidContext(), AppDatabase::class.java,
+            "appDatabase"
         ).build()
     }
 
--- app/src/main/java/com/secbreel/notes/persistance/AppDatabase.kt
+++ app/src/main/java/com/secbreel/notes/persistance/AppDatabase.kt
@@ -3,13 +3,15 @@ package com.secbreel.notes.persistance
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.secbreel.notes.model.Category
+import com.secbreel.notes.model.Note
 
 @Database(
-    entities = [Category::class],
+    entities = [Category::class, Note::class],
     version = 1,
     exportSchema = false
 )
 
-abstract class CategoryDatabase : RoomDatabase(){
+abstract class AppDatabase : RoomDatabase(){
     abstract fun categoryDao() : CategoryDAO
+    abstract fun noteDao() : NoteDAO
 }
